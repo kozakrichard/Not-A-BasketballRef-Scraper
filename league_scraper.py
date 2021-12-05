487f1dcc33fdd05015acaf5602c5c71bc67776d0
Richard Kozak <kozakrichard@users.noreply.github.com>
2021-12-05 18:46:34 -0500

    notes on how to use, simplified input for statistics

--- league_scraper.py
+++ league_scraper.py
@@ -1,13 +1,37 @@
-import re
+###This scraper works to collect the league wide stats for each team per season. This data is compiled into a csv with each column
+###as one statistic, and a row for each team.
+###
+###all_awards is the only exception, this collects the winners of the highest league awards, MVP, DPOTY, etc...
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import os
 
 #For now, enter one of these into the stats_type variable
 #totals-team, advanced-team, per_game-team, per_poss-team, shooting-team, all_awards
 
-stats_type = "per_poss-team"
+stat_to_track = int(input(
+    "What statistics would you like to track? Enter 1 for totals. 2 for advanced. 3 for per-game. 4 for per-100 possessions. 5 for shooting. 6 for awards. "
+    ))
+
+while stat_to_track != 1 and stat_to_track != 2 and stat_to_track != 3 and stat_to_track != 4 and stat_to_track != 5 and stat_to_track != 6:
+    stat_to_track = int(input("Let's try that again. Enter a valid choice between 1 and 6."))
+
+if stat_to_track == 1:
+    stats_type = "totals-team"
+elif stat_to_track == 2:
+    stats_type = "advanced-team"
+elif stat_to_track == 3:
+    stats_type = "per_game-team"
+elif stat_to_track == 4:
+    stats_type = "per_poss-team"
+elif stat_to_track == 5:
+    stats_type = "shooting-team"
+elif stat_to_track == 6:
+    stats_type = "all_awards"
+#    while stat_to_track != 1 and stat_to_track != 2 and stat_to_track != 3 and stat_to_track != 4 and stat_to_track != 5 and stat_to_track != 6:
+    #stat_to_track = int(input("Let's try that again. Enter a valid choice between 1 and 6."))
+
 
 start_year = int(input("Enter year to begin scraping from. Begins from 1950. Data will include this year: "))
 while start_year < 1950 or start_year > 2022:
