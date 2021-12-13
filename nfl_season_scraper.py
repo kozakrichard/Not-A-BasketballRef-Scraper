a5a1189b52eaf35d9b11af9f96e6a91095ccaab8
Richard Kozak <kozakrichard@users.noreply.github.com>
2021-12-13 15:20:53 -0500

    nfl season stats added

--- nfl_season_scraper.py
+++ nfl_season_scraper.py
@@ -6,16 +6,15 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import time
 
 #For now, enter one of these into the stats_type variable
 #totals-team, advanced-team, per_game-team, per_poss-team, shooting-team, all_awards
-
+"""
 stat_to_track = int(input(
     "What statistics would you like to track? Enter 1 for totals. 2 for advanced. 3 for per-game. 4 for per-100 possessions. 5 for shooting. 6 for awards. "
     ))
 
-while stat_to_track != 1 and stat_to_track != 2 and stat_to_track != 3 and stat_to_track != 4 and stat_to_track != 5 and stat_to_track != 6:
+ while stat_to_track != 1 and stat_to_track != 2 and stat_to_track != 3 and stat_to_track != 4 and stat_to_track != 5 and stat_to_track != 6:
     stat_to_track = int(input("Let's try that again. Enter a valid choice between 1 and 6."))
 
 if stat_to_track == 1:
@@ -32,7 +31,8 @@ elif stat_to_track == 6:
     stats_type = "all_awards"
 #    while stat_to_track != 1 and stat_to_track != 2 and stat_to_track != 3 and stat_to_track != 4 and stat_to_track != 5 and stat_to_track != 6:
     #stat_to_track = int(input("Let's try that again. Enter a valid choice between 1 and 6."))
-
+"""
+stats_type = "team_stats"
 
 start_year = int(input("Enter year to begin scraping from. Begins from 1950. Data will include this year: "))
 while start_year < 1950 or start_year > 2022:
@@ -45,7 +45,8 @@ while end_year < 1950 or end_year > 2023:
 
 
 for year in range(start_year, end_year):
-    url = f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_{year}.html&div=div_{stats_type}'
+    print(year)
+    url = f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=pfr&url=%2Fyears%2F{year}%2F&div=div_{stats_type}'
     page = requests.get(url)
 
     if page.status_code==200:
@@ -57,8 +58,7 @@ for year in range(start_year, end_year):
     df = pd.read_html(str(table))[0]
 
 #A csv file containing the data scraped will be created in this working directory
-    df.to_csv(f"NBA{year} {stats_type} totals.csv")
-
+    df.to_csv(f"NFL{year} {stats_type} totals.csv")
 
 
 
